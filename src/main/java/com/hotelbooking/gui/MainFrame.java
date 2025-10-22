66387e5bdb40709d40e22eec5baa4131d21121f3
Akshay07072007 <akshaysrajan2007@gmail.com>
2025-10-22 20:16:44 +0530

    Create MainFrame as main application window with navigation menu

--- src/main/java/com/hotelbooking/gui/MainFrame.java
+++ src/main/java/com/hotelbooking/gui/MainFrame.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
+/** 
  * Main GUI Frame for Hotel Booking System
  * Demonstrates OOP with GUI components
  */
